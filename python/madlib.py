9b3262030d579b1f92edd2f19dda528f03abe2c4
Zack Williams <wil18020@byui.edu>
2022-01-08 19:37:07 -0700

    Madlib added

---
+++ python/madlib.py
@@ -0,0 +1,13 @@
+#Create a Madlib story
+
+#These are the Words that
+#the user will be prompted for
+
+adjective= input("Give me an adjective: ")
+animal= input("Now, an animal: ")
+verb_1= input("Ok, now a verb: ")
+exclamation= input("Give me an exclamation: ")
+verb_2= input("I need another verb: ")
+verb_3= input("One more verb please: ")
+
+print(f'\nYour story is:\n\nThe other day, I was really in trouble. It all started when I saw a very\n{adjective} {animal} {verb_1} down the hallway."{exclamation}" I yelled. But all\nI could think to do was to {verb_2} over and over. Miraculously,\nthat caused it to stop, but not before it tried to {verb_3} right in front of my family.')
