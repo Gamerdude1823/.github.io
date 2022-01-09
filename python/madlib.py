0e123f47f8b1a9bfad7cebaedd12d9c5a5db7e4a
Zack Williams <wil18020@byui.edu>
2022-01-08 19:40:04 -0700

    Edited Madlib

--- python/madlib.py
+++ python/madlib.py
@@ -10,4 +10,9 @@ exclamation= input("Give me an exclamation: ")
 verb_2= input("I need another verb: ")
 verb_3= input("One more verb please: ")
 
-print(f'\nYour story is:\n\nThe other day, I was really in trouble. It all started when I saw a very\n{adjective} {animal} {verb_1} down the hallway."{exclamation}" I yelled. But all\nI could think to do was to {verb_2} over and over. Miraculously,\nthat caused it to stop, but not before it tried to {verb_3} right in front of my family.')
+#Includes words given by the user
+#to the story.
+
+print(f'\nYour story is:\n\nThe other day, I was really in trouble. It all started when I saw a very\n{adjective} {animal} {verb_1} \
+ down the hallway."{exclamation}" I yelled. But all\nI could think to do was to {verb_2} over and over. Miraculously,\nthat caused it to stop, \
+ but not before it tried to {verb_3} right in front of my family.')
